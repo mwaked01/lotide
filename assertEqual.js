6eca5a960838f9c978484eb1e421ea082ff4dfcd
Mohamed Waked <mwaked01@gmail.com>
2023-07-15 11:08:49 -0400

    function implementation. printing result with emojis

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// Implement the assertEqual function which comapres if the given arguments are equal.
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
