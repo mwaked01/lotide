1409576bc544fad5a7e159b119b5374135d06c6c
Mohamed Waked <mwaked01@gmail.com>
2023-07-17 12:31:15 -0400

    impliment function that takes in a sentance and returns the number count of each letter in that sentance as an object

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentance) {
+  let letters = {}; //object conataing the letters as properties with number of times ech property appears in sentance
+
+  for (let i of sentance) { //cycle through each letter in the string held by 'sentance'
+    if (i !== ' ') { //skips over empty spaces
+      if (letters[i]) { //checks if the letter is already a key in the object 'letters'
+        letters [i]++;// if teh key already exists, increase the count by 1
+      } else {// set the initial value of each key to one
+        letters [i] = 1;
+      }
+    }
+  }
+  return letters;
+};
+
+const sentanceString = "this is a sentance";
+const sentanceString1 = "lighthouse in the house";
+
+console.log(countLetters(sentanceString1));
+assertEqual(countLetters(sentanceString)["s"],3);
+assertEqual(countLetters(sentanceString)["m"],undefined);
