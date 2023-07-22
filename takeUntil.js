2fa0753c8449a229a8f045aa8a7f45ebcaf04b31
Mohamed Waked <mwaked01@gmail.com>
2023-07-22 14:12:34 -0400

    implement takeUntil to return an array containing the first items of an array up until the callback function condition is met

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+const eqArrays = function(firstArray, secondArray) {//checks if 2 arrays are identical in type and value
+  let pass = 0; // keeps track of how many elements are identical at the same index
+  if (firstArray.length === secondArray.length) {//first make sure that the arrays are the same length
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {//returns false if the elemnts are not identical
+        return false;
+      } else {
+        pass++;
+      }
+    }
+    if (pass === firstArray.length) {
+      return true;//once all the lements have been checked as identical, returns true
+    }
+  } else {
+    return false;// returns false right away if the arrays are not the same length
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  const results = []; // hold values true to callback function
+  for (let i of array) {//cycle through the items of the array
+    if (!callback(i)) {
+      results.push(i);
+    } else { //stop cycling through the array once the callback function condition is met
+      return results;
+    }
+  }
+};
+//test
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
