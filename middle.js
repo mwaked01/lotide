5547299480619218344ea2857723404b3117cc29
Mohamed Waked <mwaked01@gmail.com>
2023-08-02 23:58:20 -0400

    refactored middle.js by importing its dependancies and exporting it to be used by its test file in the test folder.

--- middle.js
+++ middle.js
@@ -1,28 +1,3 @@
-const eqArrays = function(firstArray,secondArray) {//checks if 2 arrays are identical in type and value
-  let pass = 0; // keeps track of how many elements are identical at the same index
-  if (firstArray.length === secondArray.length) {//first make sure that the arrays are the same length
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] !== secondArray[i]) {//returns false if the elemnts are not identical
-        return false;
-      } else {
-        pass ++;
-      }
-    }
-    if (pass === firstArray.length) {
-      return true;//once all the lements have been checked as identical, returns true
-    }
-  } else {
-    return false;// returns false right away if the arrays are not the same length
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual,expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const middle = function(arr) {
   let resultArr = [];//holds the middle elements of the arr array
@@ -38,14 +13,4 @@ const middle = function(arr) {
   return resultArr;
 };
 
-//test
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
-assertArraysEqual(middle([1,2,'4',4,5]),['4']);
-assertArraysEqual(middle([]),[]);
-assertArraysEqual(middle([1,2,3,4,5,6,true,8,9,0,11,12,13]),[true]);
-
-
+module.exports = middle;
