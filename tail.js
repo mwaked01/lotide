c5a33de4386e6b4ea001736e3d197e13bf55f9ef
Mohamed Waked <mwaked01@gmail.com>
2023-08-02 23:32:56 -0400

    rafctored tails.js and dried up the code by moving the test to the test folder

--- tail.js
+++ tail.js
@@ -1,22 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(arr) {
   let newArr = arr.slice(1); // adds all the elemnts of the array to newArr except for the first element
   return newArr;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-console.log(result);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", 61 , "Labs",5];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const tail = require ('../tail');
+const assertEqual = require ('../assertEqual');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result[0], "Lighthouse");
+
+console.log(result);
+
+// Test Case: Check the original array
+const words = ["Yo Yo", 61 , "Labs",5];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 4);
