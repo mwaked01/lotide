68c5a17a79ba621b18dcc8441908b1cf3319578e
Mohamed Waked <mwaked01@gmail.com>
2023-08-02 23:45:17 -0400

    refactored the assertArraysEqual.js file by importing the dependancies and exporting it to be used in its test file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,4 @@
-const eqArrays = function(firstArray,secondArray) {//checks if 2 arrays are identical in type and value
-  let pass = 0; // keeps track of how many elements are identical at the same index
-  if (firstArray.length === secondArray.length) {//first make sure that the arrays are the same length
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] !== secondArray[i]) {//returns false if the elemnts are not identical
-        return false;
-      } else {
-        pass ++;
-      }
-    }
-    if (pass === firstArray.length) {
-      return true;//once all the lements have been checked as identical, returns true
-    }
-  } else {
-    return false;// returns false right away if the arrays are not the same length
-  }
-};
-
+const eqArrays = require ('./eqArrays');
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays (actual,expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -24,5 +7,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1], ['1']);
+module.exports = assertArraysEqual;
+
